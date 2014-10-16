50113bc97e08727c84febd0407245c9533ea8f36
vagrant <vagrant@rails-dev-box>
2014-10-16 14:39:06 +0000

    create a new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,12 @@
 class ListsController < ApplicationController
+  def new
+  end
+  def create
+    @list = List.new(params.require(:list).permit(:title, :description))
+    @list.save
+    redirect_to @list, notice: "List was created!"
+  end
+  def show
+    @list = List.find(params[:id])
+  end
 end
--- spec/features/create_list_spec.rb
+++ spec/features/create_list_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 feature 'Successfully create a single to-do list' do 
   scenario 'Successfully' do
     visit new_list_path
+    fill_in('Title', with: 'First List')
+    fill_in('Description', with: 'Create lists controller, list.rb, associated views')
+    click_button('Save')
+    expect( page ).to have_content("List was created!")
+    expect( page ).to have_content("First List")
+    expect( page ).to have_content("Create lists controller, list.rb, associated views")
   end
 end
