82b125f947070e2f27785702e9c364e72fbdab52
vagrant <vagrant@rails-dev-box>
2014-10-17 18:46:46 +0000

    refactor views: index, show, new

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,24 @@
 class ListsController < ApplicationController
-  def new
+  
+  def index
+    @lists = List.all
   end
+
+  def new
+    @list = List.new  
+  end
+
   def create
     @list = List.new(params.require(:list).permit(:title, :description))
-    @list.save
-    redirect_to @list, notice: "List was created!"
+
+    if @list.save
+      redirect_to @list, notice: "List was created!"
+    else
+      render :new
+    end
+    
   end
+
   def show
     @list = List.find(params[:id])
   end
